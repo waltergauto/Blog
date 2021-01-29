66bf00794ed271021315f9b33f134c6b81c3a958
Walter Gauto <waltergautofcb@gmail.com>
2021-01-29 10:06:00 -0300

    Se agregan las rutas para cada una de las vistas

--- Blog/apps/blog/urls.py
+++ Blog/apps/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import home
+from .views import *
 
 urlpatterns = [
     path('', home, name = 'index'),
+    path('generales/', generales, name = 'generales'),
+    path('redes/', redes, name = 'redes'),
+    path('videojuegos/', videojuegos, name = 'videojuegos'),
+    path('programacion/', programacion, name = 'programacion'),
+    path('<slug:slug>/', detallePost, name = 'detalle_post')
 ]
